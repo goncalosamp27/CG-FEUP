1d4e9aa304de1f51ab4364d12909bc40454c9f98
Proenca13 <henriqueproenca2004@hotmai.com>
2025-02-27 11:14:44 +0000

    error corrected

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -2,8 +2,8 @@ import { CGFobject } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
-import { MyTriangleSmall } from "./MyTriangleSmall.js";
-import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTriangleSmall } from "./myTriangleSmall.js";
+import { MyTriangleBig } from "./myTriangleBig.js";
 
 export class MyTangram extends CGFobject {
   constructor(scene) {
